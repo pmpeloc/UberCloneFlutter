92061751aa427c528f4c8470859a71f84dd1bb22
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-08 20:14:28 -0300

    feat: Custom Clippers and gradients added

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,37 +8,68 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset('assets/img/logo_app.png',
-                  width: 150,
-                  height: 100,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.black, Colors.black87]
+            )
+          ),
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: DiagonalPathClipperTwo(),
+                child: Container(
+                  color: Colors.white,
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset('assets/img/logo_app.png',
+                        width: 150,
+                        height: 100,
+                      ),
+                      Text('Fácil y Rápido')
+                    ],
+                  ),
                 ),
-                Text('Fácil y Rápido')
-              ],
-            ),
-            SizedBox(height: 50),
-            Text('SELECCIONA TU ROL'),
-            SizedBox(height: 30),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/pasajero.png'),
-              radius: 50,
-              backgroundColor: Colors.black,
-            ),
-            SizedBox(height: 10,),
-            Text('Cliente'),
-            SizedBox(height: 30),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/driver.png'),
-              radius: 50,
-              backgroundColor: Colors.black,
-            ),
-            SizedBox(height: 10,),
-            Text('Conductor'),
-          ],
+              ),
+              SizedBox(height: 50),
+              Text(
+                'SELECCIONA TU ROL',
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+              SizedBox(height: 30),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/img/pasajero.png'),
+                radius: 50,
+                backgroundColor: Colors.grey[900],
+              ),
+              SizedBox(height: 10,),
+              Text(
+                'Cliente',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+              SizedBox(height: 30),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/img/driver.png'),
+                radius: 50,
+                backgroundColor: Colors.grey[900],
+              ),
+              SizedBox(height: 10,),
+              Text(
+                'Conductor',
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
