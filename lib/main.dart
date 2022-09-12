0bfe9d7796050d7238aaccbd70300fa61177ab57
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-12 13:12:21 -0300

    feat: Login Page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:uber_clone_flutter/pages/home/home_page.dart';
 import 'package:uber_clone_flutter/pages/login/login_page.dart';
+import 'package:uber_clone_flutter/utils/colors.dart' as utils;
 
 void main() {
   runApp(const MyApp());
@@ -21,12 +24,38 @@ class _MyAppState extends State<MyApp> {
       title: 'Uber Clone',
       initialRoute: 'home',
       theme: ThemeData(
-        fontFamily: 'NimbusSans'
+        fontFamily: 'NimbusSans',
+        appBarTheme: AppBarTheme(
+          elevation: 0
+        ),
+        primarySwatch: createThemeSwatch(utils.Colors.uberCloneColor),
+        // primaryColor: utils.Colors.uberCloneColor,
       ),
       routes: {
         'home': (BuildContext context) => HomePage(),
         'login': (BuildContext context) => LoginPage(),
       },
     );
+  }
+
+  // create theme swatch
+  MaterialColor createThemeSwatch(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    strengths.forEach((strength) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    });
+    return MaterialColor(color.value, swatch);
   }
 }
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:uber_clone_flutter/utils/colors.dart' as utils;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,8 +15,121 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            _bannerApp(),
+            _textDescription(),
+            _textLogin(),
+            Expanded(child: Container()),
+            _textFieldEmail(),
+            _textFieldPassword(),
+            _buttonLogin(),
+            _textDontHaveAccount()
+          ],
+        ));
+  }
+
+  Widget _textDontHaveAccount() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 50),
+      child: Text(
+        'No tienes cuenta?',
+        style: TextStyle(fontSize: 15, color: Colors.grey),
+      ),
+    );
+  }
+
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('Iniciar Sesión'),
+      ),
+    );
+  }
+
+  Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'mail@mail.com',
+            labelText: 'Correo Electrónico',
+            suffixIcon: Icon(
+              Icons.email_outlined,
+              color: utils.Colors.uberCloneColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: 'Contraseña',
+            suffixIcon: Icon(
+              Icons.lock_open_outlined,
+              color: utils.Colors.uberCloneColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _textLogin() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: Text(
+        'Login',
+        style: TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 28),
+      ),
+    );
+  }
+
+  Widget _textDescription() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Continua con tu',
+        style: TextStyle(
+            color: Colors.black54, fontSize: 24, fontFamily: 'NimbusSans'),
+      ),
+    );
+  }
+
+  Widget _bannerApp() {
+    return ClipPath(
+      clipper: WaveClipperTwo(),
+      child: Container(
+        color: utils.Colors.uberCloneColor,
+        height: MediaQuery.of(context).size.height * 0.22,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'assets/img/logo_app.png',
+              width: 150,
+              height: 100,
+            ),
+            Text(
+              'Fácil y Rápido',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
